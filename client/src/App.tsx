4cc152a755bf3042aeecc8f869978999b3c90f2c
oleg krotenko <olk@sitecore.net>
2022-06-16 23:38:34 +0300

    add mui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,36 @@
 import { Data } from '../types/data';
 import { DataRow } from './components/data.row';
 import { useData } from './hooks/data.hook';
-
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { useNavigate } from "react-router-dom";
+import { KerimAppBar } from './components/app.bar';
 function App() {
   const data: Data[] = useData();
+  let navigate = useNavigate();
+
+  const goToDetails = (id: string) => {
+    navigate(`/details/${id}`);
+  }
+
   return (
     <div>
-      Hello my friend
-      <div>
+      <KerimAppBar />
+
+      <List component="nav" aria-label="secondary mailbox folder">
         {
-          data.map(el => <DataRow data={el} />)
+          data.map((el, index) => (
+            <ListItemButton
+              key={index}
+              onClick={(event) => goToDetails(el.id)}
+            >
+              <ListItemText primary={el.header} />
+            </ListItemButton>
+          ))
         }
-      </div>
+      </List>
     </div>
   );
 }
--- client/src/Detail.tsx
+++ client/src/Detail.tsx
@@ -1,6 +1,9 @@
 import { Data } from './../types/data'
 import { useData } from './hooks/data.hook';
 import { useParams } from "react-router-dom";
+import Divider from '@mui/material/Divider';
+import { KerimAppBar } from './components/app.bar';
+
 type Props = {
     data: Data
 }
@@ -9,12 +12,14 @@ export function Detail() {
     let params = useParams();
     const dataList: Data[] = useData();
     const data = dataList.find(x => x.id == params.id);
-    
+
     return (
-        <div style={{ backgroundColor: '#dfdfdf' }}>
-            Detail
+        <div>
+            <KerimAppBar />
+
             <h1>{data?.header}</h1>
-            <h3>{data?.text}</h3>
+            <Divider />
+            <p>{data?.text}</p>
         </div>
     )
 }
